bf3fc79916c39f82db4261d93bcdf8da0b69933e
nicolargo <nicolashennion@gmail.com>
2016-03-31 20:39:31 +0200

    Multithread for get IP address

--- ippublic/ippublic.py
+++ ippublic/ippublic.py
@@ -1,19 +1,27 @@
-from urllib2 import urlopen
-my_ip = urlopen('http://ip.42.pl/raw').read()
-print(my_ip)
-
+import Queue
+import threading
 from json import load
 from urllib2 import urlopen
-my_ip = load(urlopen('http://jsonip.com'))['ip']
-print(my_ip)
 
 
-from json import load
-from urllib2 import urlopen
-my_ip = load(urlopen('http://httpbin.org/ip'))['origin']
-print(my_ip)
+# called by each thread
+def get_ip_public(q, url):
+    if url == 'http://ip.42.pl/raw':
+        q.put(urlopen(url).read())
+    elif url == 'http://httpbin.org/ip':
+        q.put(load(urlopen(url))['origin'])
+    elif url == 'http://jsonip.com' or url == 'https://api.ipify.org/?format=json':
+        q.put(load(urlopen('http://jsonip.com'))['ip'])
 
-from json import load
-from urllib2 import urlopen
-my_ip = load(urlopen('https://api.ipify.org/?format=json'))['ip']
-print(my_ip)
+urls = ['http://ip.42.pl/raw', 'http://httpbin.org/ip', 'http://jsonip.com', 'https://api.ipify.org/?format=json']
+
+
+q = Queue.Queue()
+
+for u in urls:
+    t = threading.Thread(target=get_ip_public, args=(q, u))
+    t.daemon = True
+    t.start()
+
+ip = q.get()
+print(ip)
