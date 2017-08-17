6567b4bbf5428fa73e63611af45115ccf38d15cb
nicolargo <nicolas@nicolargo.com>
2017-08-17 15:12:29 +0200

    Add GlancesArena a testbed for Glances development

---
+++ glancesarena/glances3/plugin.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+#
+# POC for Glances 3 (Core)
+#
+# Nicolargo (08/2017)
+
+from threading import Lock, Event
+from functools import wraps
+
+from time import time, sleep
+import random
+
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+
+
+class Timer(object):
+
+    """The timer class. A simple chronometer."""
+
+    def __init__(self, duration):
+        self.duration = duration
+        self.start()
+
+    def start(self):
+        self.target = time() + self.duration
+
+    def reset(self):
+        self.start()
+
+    def get(self):
+        return self.duration - (self.target - time())
+
+    def set(self, duration):
+        self.duration = duration
+
+    def finished(self):
+        return time() > self.target
+
+
+def lockandrun(function):
+    """
+    Decorator for method. Return a new method with the
+    an acquired lock (self._lock), run it and releases
+    the lock.
+    """
+    @wraps(function)
+    def wrapper(self, *args, **kwargs):
+        with self._lock:
+            return function(self, *args, **kwargs)
+    return wrapper
+
+
+class Plugin:
+    def __init__(self, name):
+        logging.info('Init plugin {}'.format(name))
+        # Event needed to stop properly the thread
+        self._stopper = Event()
+        # The lock for the current thread
+        self._lock = Lock()
+        # Pugin's name
+        self._name = name
+        # Plugin's stats
+        self._stats = []
+
+    def stop(self, timeout=None):
+        """Stop the thread"""
+        self._stopper.set()
+
+    def stopped(self):
+        """Return True is the thread is stopped"""
+        return self._stopper.isSet()
+
+    @lockandrun
+    def update(self, item):
+        """Simulate an plugin update method."""
+        s = random.uniform(0, 10)
+        logging.info('Update processe started')
+        self._stats.append(item)
+        t = Timer(s)
+        while not t.finished() and not self.stopped():
+            sleep(0.01)
+        if self.stopped():
+            logging.info('Update processes stopped')
+        else:
+            logging.info('Update method done')
---
+++ glancesarena/glances3/test01.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# POC for Glances 3 (Core)
+#
+# Nicolargo (08/2017)
+
+import signal
+import sys
+import threading
+from plugin import Plugin
+
+
+class TestPlugin(Plugin):
+    pass
+
+
+class Stats(object):
+
+    def __init__(self):
+        # List of Plugin classes
+        self.plugins_list = []
+
+        # List of threads
+        self.plugin_threads = []
+
+        # Init plugins
+        for i in range(1, 10):
+            self.plugins_list.append(Plugin(name='plugin%s' % (i - 1)))
+
+    def update(self):
+        self.plugin_threads = []
+        for i in range(0, 9):
+            t = threading.Thread(name='plugin%s' % i,
+                                 target=self.plugins_list[i].update,
+                                 args=(i,))
+            self.plugin_threads.append(t)
+            t.start()
+
+    def stop(self, signal, frame):
+        print('CTRL-C pressed')
+        for i in range(0, 9):
+            self.plugins_list[i].stop()
+
+
+def main():
+    s = Stats()
+    signal.signal(signal.SIGINT, s.stop)
+    s.update()
+
+
+if __name__ == '__main__':
+    main()
